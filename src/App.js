e2cf65ce77998bfb5bb5f15a9bba9e820db494a8
Ganeh Pawar <ganesh.pawar@vshift.com>
2020-03-13 17:30:37 +0530

    new dropdown component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import { Router } from "@reach/router"
-import ButtonPage from './pages/button-page';
+import React from "react";
+import { Router } from "@reach/router";
+import ButtonPage from "./pages/button-page";
+import DropdownPage from "./pages/dropdown-page";
+
 function App() {
   return (
     <Router>
-      <ButtonPage path="/"/>
+      <ButtonPage path="/" />
+      <DropdownPage path="/dropdown" />
     </Router>
   );
 }
